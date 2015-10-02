e44e57854e83cb6781dc757577e528800ffd309e
mgy <marc.gyongyosi@gmail.com>
2015-10-01 19:08:04 -0500

    new

--- hw1/decisiontree_new.py
+++ hw1/decisiontree_new.py
@@ -3,64 +3,12 @@ import sys
 import random
 import math
 
-def calc_E(freq_true, freq_false):
-		if ((freq_true == 0) | (freq_false == 0)):
-			return 0
-		else:
-			try:
-				return -1*(float(freq_true)*math.log(float(freq_true), 2)) - (float(freq_false)*math.log(float(freq_false), 2))
-			except:
-				print "freq_true: " + str(freq_true)
-				print "freq_false: " + str(freq_false)
+class node:
+	parent = ""
+	trueChild = ""
+	falseChihld = ""
 
-class Attribute:
-	informationGain = 0
-	entropyToTarget = 0
-	name = ""
-	index_in_data = 0
-	def __init__(self, name, tt, tf, ft, ff, entropyOfTarget, index):
-		#print "In Attribute: "
-		total = tt+tf+ft+ff
-		self.name = name
-		"""
-		print "total: "
-		print total
-		print "tt: "
-		print tt
-		print "tf: "
-		print tf
-		print "ft: "
-		print ft
-		print "ff: "
-		print ff
-		"""
-		self.index_in_data = index
-		
-		if ((tt+tf) == 0):
-			P_tt = 0.
-			P_tf = 0.
-		else: 
-			P_tt = float(tt)/float(tt+tf)	
-			P_tf = float(tf)/float(tt+tf)
 
-		if ((ft+ff) == 0):
-			P_ft = 0.
-			P_ff = 0.
-		else: 
-			P_ft = float(ft)/float(ft+ff)	
-			P_ff = float(ff)/float(ft+ff)
-
-				
-		
-		self.entropyToTarget = float(tt+tf)/float(total) * calc_E(P_tt, P_tf) + float(ft+ff)/float(total) * calc_E(P_ft, P_ff)
-		self.informationGain = entropyOfTarget - self.entropyToTarget
-	def print_me(self):
-		print "Attribute Name: " + self.name
-		print "\t Information Gain: " + str(self.informationGain)
-		print "\t Entropy w.r.t target: " + str(self.entropyToTarget)
-		print "\t Index in data: " + str(self.index_in_data)
-		
-		
 
 class ID3Solver:
 	inputFileName = ""
@@ -98,54 +46,8 @@ class ID3Solver:
 		print "Number of trials: " + str(self.numberOfTrials)
 		if (self.verbose):
 			print "RUNNING VERBOSE MODE"
-		self.runTree()
-
-	def runTree(self):
 		f = open(self.inputFileName, 'rb');
 		self.reader = csv.reader(f, delimiter='\t')
-		self.process_text()
-		self.calc_prior()
-		self.calc_E_T()	
-		index = 0
-		for x in self.categories:
-			tt = 0 
-			tf = 0
-			ft = 0
-			ff = 0
-			for y in self.trainingSet:
-				if (y[index] == "true "):
-					if (y[len(self.categories)] == "true"):
-						tt += 1
-					elif (y[len(self.categories)] == "false"):
-						tf += 1
-				elif (y[index] == "false "):
-					if (y[len(self.categories)] == "true"):
-						ft += 1
-					elif (y[len(self.categories)] == "false"):
-						ff += 1
-				else:
-					sys.exit("Error 2, unidentified classifier")
-			self.attributes.append(Attribute(x, tt, tf, ft, ff, self.entropyOfTarget,index))
-			index += 1
-			
-		for element in self.attributes:
-			element.print_me()
-
-		
-		f.close() 
-		del self.trainingSet[:]
-		del self.testingSet[:]
-		del self.categories[:]
-		del self.attributes[:]
-		
-				
-				
-	
-	def print_input(self):
-		for row in self.reader:
-			print row
-	
-	def process_text(self):
 		try:
 			self.categories = next(self.reader)
 			del self.categories[-1]
@@ -161,6 +63,22 @@ class ID3Solver:
 			print "size of all data: " + str(self.dataSize)
 		except: 
 			sys.exit("Error processing text file")
+
+		print self.categories
+		print self.trainingSet
+		
+		self.calc_prior()
+
+		
+		###call the recursive function in here
+
+
+
+		f.close() 
+		del self.trainingSet[:]
+		del self.testingSet[:]
+		del self.categories[:]
+		del self.attributes[:]
 
 	def calc_prior(self):
 		#print self.trainingSet
@@ -179,15 +97,11 @@ class ID3Solver:
 			print "something went wrong. PP true: " + str( self.expectedPPTrue) + " PP wrong: " + str(self.expectedPPFalse)
 		print "Prior Probability for TRUE: " + str(self.expectedPPTrue*100.) + "%"
 		print "Prior Probability for FALSE: " + str(self.expectedPPFalse*100.) + "%"
+
 		
-	def calc_E_T(self):
-		freq_true = float(self.numberTrue)/float(self.trainingSetSize)
-		freq_false = float(self.numberFalse)/float(self.trainingSetSize)
-		self.entropyOfTarget = calc_E(freq_true, freq_false)
-		print "Entropy of Target Concept: " + str(self.entropyOfTarget)
 
 
-	
+
 
 
 
