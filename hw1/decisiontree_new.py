9043f23fa66d68f23128a13b917574bd91206816
MarcGyongyosi <marc.gyongyosi@u.northwestern.edu>
2015-10-02 02:20:33 -0500

    only missin prior prob

--- hw1/decisiontree_new.py
+++ hw1/decisiontree_new.py
@@ -3,7 +3,7 @@ import sys
 import random
 import math
 
-tree = []
+gen_info_printed = False
 
 def calc_E(freq_true, freq_false):
     if ((freq_true == 0) | (freq_false == 0)):
@@ -176,21 +176,24 @@ class ID3Solver:
 	expectedPPFalse = 0;
 
 	entropyOfTarget = 0;
+	
+	id3performance = 0;
 
-	attributes = []
+	
 
 	
 	def __init__(self, inputFileName, trainingSetSize, numberOfTrials, verbose):
-		global tree
+		global gen_info_printed
 		self.inputFileName = inputFileName
 		self.trainingSetSize = trainingSetSize
 		self.numberOfTrials = numberOfTrials
 		self.verbose = verbose
 		
-		print "Starting ID3 Solver with following arguments:"
-		print "Input file: " + self.inputFileName
-		print "Trainingset size: " + str(self.trainingSetSize)
-		print "Number of trials: " + str(self.numberOfTrials)
+		if not gen_info_printed:
+			print "Starting ID3 Solver with following arguments:"
+			print "Input file: " + self.inputFileName
+			print "Trainingset size: " + str(self.trainingSetSize)
+			print "Number of trials: " + str(self.numberOfTrials)
 		if (self.verbose):
 			print "RUNNING VERBOSE MODE"
 		f = open(self.inputFileName, 'rb');
@@ -205,16 +208,25 @@ class ID3Solver:
 				self.trainingSet.append(entire_data_list[rand_i])
 				del entire_data_list[rand_i]
 			self.testingSet = entire_data_list
-			print "size of testing set: " + str(len(self.testingSet))
-			print "size of training set: " + str(len(self.trainingSet))
-			print "size of all data: " + str(self.dataSize)
+			if not gen_info_printed:
+				print "size of testing set: " + str(len(self.testingSet))
+				print "size of training set: " + str(len(self.trainingSet))
+				print "size of all data: " + str(self.dataSize)
 		except: 
 			sys.exit("Error processing text file")
-		if (verbose):
+		if (self.verbose):
+			print "Categories: "
 			print self.categories
+			print "Training Set: "
 			print self.trainingSet
+			print "Testing Set: "
+			print self.testingSet
 		#calculate prior probability
 		self.calc_prior()
+		if not gen_info_printed:
+			print "Prior Probability for TRUE: " + str(self.expectedPPTrue*100.) + "%"
+			print "Prior Probability for FALSE: " + str(self.expectedPPFalse*100.) + "%"
+			gen_info_printed = True
 		#calculate entropy of target
 		self.calc_E_T(); 
 		
@@ -225,7 +237,7 @@ class ID3Solver:
 		
 		print self.print_node(rootNode)
 		
-		print "testing performance"
+		#print "testing performance"
 		
 		testCount = 0
 		goodCount = 0
@@ -237,14 +249,14 @@ class ID3Solver:
 				badCount += 1
 			testCount += 1
 		
-		print "Total Tests:" + str(testCount) + " Good: " + str(goodCount) + " Bad: " + str(badCount)
+		#print "Total Tests:" + str(testCount) + " Good: " + str(goodCount) + " Bad: " + str(badCount)
+		self.id3performance = 100*float(goodCount)/float(testCount)
 
 
 		f.close()
 		del self.trainingSet[:]
 		del self.testingSet[:]
 		del self.categories[:]
-		del self.attributes[:]
 		
 	def createTree(self, local_trainingSet, local_categories, parent):
 		self.entropyOfTarget = update_E_T(local_trainingSet)
@@ -301,8 +313,8 @@ class ID3Solver:
 		self.expectedPPFalse = float(self.numberFalse)/float(self.trainingSetSize)
 		if ((self.expectedPPTrue+self.expectedPPFalse) != 1.):
 			print "something went wrong. PP true: " + str( self.expectedPPTrue) + " PP wrong: " + str(self.expectedPPFalse)
-		print "Prior Probability for TRUE: " + str(self.expectedPPTrue*100.) + "%"
-		print "Prior Probability for FALSE: " + str(self.expectedPPFalse*100.) + "%"
+		#print "Prior Probability for TRUE: " + str(self.expectedPPTrue*100.) + "%"
+		#print "Prior Probability for FALSE: " + str(self.expectedPPFalse*100.) + "%"
 
 	def calc_E_T(self):
 		freq_true = float(self.numberTrue)/float(self.trainingSetSize)
@@ -320,9 +332,18 @@ class ID3Solver:
 			return self.exampleCheck(example, input)
 			
 	def exampleCheck(self, example, test):
+		if (self.verbose):
+			print " ---- "
+			print "//ID3 classified example as " +str(test)
+			print "//Data reports " + str(example[-1]) 
 		if (example[-1] == test):
+			if (self.verbose):
+				print "//This is correct."
 			return 1
 		else:
+			if (self.verbose):
+				print "//This is wrong."
+				print " ---- "
 			return 0
 		
 	def lookup_example(self, example, attribute):
@@ -354,7 +375,15 @@ if __name__ == "__main__":
 				verbose = True
 		except:
 			sys.exit("The provided arguments are malformed")
+	id3performance_total = 0
 	for x in range(1,numberOfTrials+1):
+		print "----------------------------------------------------------"
 		print "TRIAL: " + str(x)
-    		solver = ID3Solver(inputFileName, trainingSetSize, numberOfTrials, verbose)
+		solver = ID3Solver(inputFileName, trainingSetSize, numberOfTrials, verbose)
+		print "                Performance of ID3 tree: " + str(solver.id3performance) + "% correct"
+		id3performance_total += float(solver.id3performance)
 		del solver
+		
+	print "_______________________________________________________________"
+	print "---------------------------------------------------------------"
+	print "        Mean Performance of ID3: " + str(float(id3performance_total)/float(numberOfTrials))
