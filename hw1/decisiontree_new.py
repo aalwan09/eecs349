0c0d99b10cde2cf1e9f3ff323048ccf809c982ba
MarcGyongyosi <marc.gyongyosi@u.northwestern.edu>
2015-10-01 23:16:54 -0500

    major update

--- hw1/decisiontree_new.py
+++ hw1/decisiontree_new.py
@@ -3,11 +3,126 @@ import sys
 import random
 import math
 
-class node:
-	parent = ""
-	trueChild = ""
-	falseChihld = ""
+tree = []
 
+def calc_E(freq_true, freq_false):
+    if ((freq_true == 0) | (freq_false == 0)):
+        return 0
+    else:
+        try:
+            return -1*(float(freq_true)*math.log(float(freq_true), 2)) - (float(freq_false)*math.log(float(freq_false), 2))
+        except:
+            print "freq_true: " + str(freq_true)
+            print "freq_false: " + str(freq_false)
+            
+def findHighestIG(local_trainingSet, local_categories, entropyOfTarget):
+	index = 0
+	attributes = []
+	max_information_gain = 0.;
+	max_information_gain_index = 0;
+	#print "trying to find highest IG"
+	
+	for x in local_categories:
+		tt = 0
+		tf = 0
+		ft = 0
+		ff = 0
+		for y in local_trainingSet:
+			if (y[index] == "true "):
+				if (y[len(local_categories)] == "true"):
+					tt += 1
+				elif (y[len(local_categories)] == "false"):
+					tf += 1
+			elif (y[index] == "false "):
+				if (y[len(local_categories)] == "true"):
+					ft += 1
+				elif (y[len(local_categories)] == "false"):
+					ff += 1
+		if ((tt+tf) == 0):
+			P_tt = 0.
+			P_tf = 0.
+		else:
+			P_tt = float(tt)/float(tt+tf)
+			P_tf = float(tf)/float(tt+tf)
+			
+		if ((ft+ff) == 0):
+			P_ft = 0.
+			P_ff = 0.
+		else:
+			P_ft = float(ft)/float(ft+ff)
+			P_ff = float(ff)/float(ft+ff)
+			
+		total = tt+tf+ft+ff
+		entropyToTarget = float(tt+tf)/float(total) * calc_E(P_tt, P_tf) + float(ft+ff)/float(total) * calc_E(P_ft, P_ff)
+		informationGain = entropyOfTarget - entropyToTarget
+		if (informationGain > max_information_gain):
+			max_information_gain = informationGain
+			max_information_gain_index = index
+		
+		#print str(local_categories[index]) + " Information Gain = " + str(informationGain)
+		index += 1
+	highestIGnode = node("null", local_categories[max_information_gain_index], "null", "null")
+	return highestIGnode
+	
+def	trueTraining(input_trainingSet, input_categories, attribute):
+	index = 0
+	#print "finding trueTrainign"
+	#print input_categories
+	for x in input_categories:
+		#print x
+		if (x == str(attribute)):
+			#print "found attribute in categories, index: " + str(index)
+			break
+		else:
+			index += 1
+	output_trainingSet = []
+	for x in input_trainingSet:
+		if (x[index] == "true "):
+			output_trainingSet.append(x[0:index] + x[index+1:len(x)])	
+	return output_trainingSet
+
+def	falseTraining(input_trainingSet, input_categories, attribute):
+	index = 0
+	for x in input_categories:
+		if (x == str(attribute)):
+			break
+		else:
+			index += 1
+	output_trainingSet = []
+	for x in input_trainingSet:
+		if (x[index] == "false "):
+			output_trainingSet.append(x[0:index] + x[index+1:len(x)])
+	return output_trainingSet
+	
+def update_categories(input_categories, attribute):
+	index = 0
+	#print "input to categories update: "
+	#print input_categories
+	#print "looking for: " + str(attribute)
+	for x in input_categories:
+		if (x == str(attribute)):
+			#print "found attribute"
+			break
+		else:
+			index += 1
+	output = (input_categories[0:index] + input_categories[index+1:len(input_categories)])
+	#print "output from categories update: "
+	#print output
+	return output
+
+class node:
+    parent = ""
+    attribute = ""
+    trueChild = ""
+    falseChild = ""
+    def __init__(self, parent, attribute, trueChild, falseChild):
+        self.parent = parent
+        self.attribute = attribute
+        self.trueChild = trueChild
+        self.falseChild = falseChild
+    def print_name():
+        print self.attribute
+        
 
 
 class ID3Solver:
@@ -35,6 +150,7 @@ class ID3Solver:
 
 	
 	def __init__(self, inputFileName, trainingSetSize, numberOfTrials, verbose):
+		global tree
 		self.inputFileName = inputFileName
 		self.trainingSetSize = trainingSetSize
 		self.numberOfTrials = numberOfTrials
@@ -63,22 +179,44 @@ class ID3Solver:
 			print "size of all data: " + str(self.dataSize)
 		except: 
 			sys.exit("Error processing text file")
-
-		print self.categories
-		print self.trainingSet
+		if (verbose):
+			print self.categories
+			print self.trainingSet
 		
 		self.calc_prior()
-
+		
+		self.calc_E_T();
+        
+		#testNode = self.findHighestIG(self.trainingSet, self.categories, self.entropyOfTarget)
+		#print testNode.attribute
 		
 		###call the recursive function in here
+		rootNode = self.createTree(self.trainingSet, self.categories, "root")
+		
+		
 
 
-
-		f.close() 
+		f.close()
 		del self.trainingSet[:]
 		del self.testingSet[:]
 		del self.categories[:]
 		del self.attributes[:]
+		
+	def createTree(self, local_trainingSet, local_categories, parent):
+		newNode = findHighestIG(local_trainingSet, local_categories, self.entropyOfTarget)
+		newNode.parent = parent
+		trueChild_trainingSet = trueTraining(local_trainingSet, local_categories, newNode.attribute)
+		falseChild_trainingSet = falseTraining(local_trainingSet, local_categories, newNode.attribute)
+		new_categories = update_categories(local_categories, newNode.attribute)
+		if ((trueChild_trainingSet != []) & (falseChild_trainingSet != [])):
+			#print "in recursion"
+			#print "new categories: "
+			#print new_categories
+			newNode.trueChild = self.createTree(trueChild_trainingSet, new_categories, newNode.attribute)
+			newNode.falseChild = self.createTree(falseChild_trainingSet, new_categories, newNode.attribute)
+			return newNode
+		else:
+			return  
 
 	def calc_prior(self):
 		#print self.trainingSet
@@ -97,6 +235,13 @@ class ID3Solver:
 			print "something went wrong. PP true: " + str( self.expectedPPTrue) + " PP wrong: " + str(self.expectedPPFalse)
 		print "Prior Probability for TRUE: " + str(self.expectedPPTrue*100.) + "%"
 		print "Prior Probability for FALSE: " + str(self.expectedPPFalse*100.) + "%"
+
+	def calc_E_T(self):
+		freq_true = float(self.numberTrue)/float(self.trainingSetSize)
+		freq_false = float(self.numberFalse)/float(self.trainingSetSize)
+		self.entropyOfTarget = calc_E(freq_true, freq_false)
+		print "Entropy of Target Concept: " + str(self.entropyOfTarget)
+		
 
 		
 
