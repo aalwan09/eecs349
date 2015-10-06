87b918a0ef247a4daf182a2b56f5299c8deb396f
mgy <marc.gyongyosi@gmail.com>
2015-10-05 21:53:42 -0500

    hw2 param test prob3 done

--- hw2/spellcheck.py
+++ hw2/spellcheck.py
@@ -3,11 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 import time
+import random
 
+
+insertionCost = 1
+deletionCost = 1
+substitutionCost = 1
 
 
 def levenshtein_distance(string1, string2, deletionCost, insertionCost, substitutionCost):
-
+	#print insertionCost
 	m = len(string1)
 	n = len(string2)
 
@@ -39,11 +44,14 @@ def levenshtein_distance(string1, string2, deletionCost, insertionCost, substitu
 	return d[m][n]
 
 def find_closest_word(string1, dictionary):
+	global insertionCost
+	global deletionCost 
+	global substitutionCost
 	min_distance = 99999999
 	closest_string = ""
 
 	for tester in dictionary:
-		distance = levenshtein_distance(string1, tester, 1,1,1)
+		distance = levenshtein_distance(string1, tester, deletionCost,insertionCost,substitutionCost)
 		if (distance < min_distance):
 			#print "found closer distance: " + string1 + "is similar to: " + tester
 			#print "distance from " + string1 + " to " + tester + "=" + str(distance)
@@ -64,41 +72,40 @@ def measure_error(typos, truewords, dictionarywords):
 	error_count = 0
 	tries = 0
 	index = 0
-	error_individual = [0 for x in range(0, len(typos))]
-	rolling_window = [0 for x in range(0,20)]
+	#error_individual = [0 for x in range(0, len(typos))]
+	#rolling_window = [0 for x in range(0,20)]
 	for typo in typos:
 		solution = find_closest_word(typo, dictionarywords) 
-		print "---------------------------------"
-		print "typo: " + typo + " correct would be: " + truewords[index] + " found: " + solution
+		#print "---------------------------------"
+		#print "typo: " + typo + " correct would be: " + truewords[index] + " found: " + solution
 		if (solution != truewords[index]):
-			print "This is WRONG"
+			#print "This is WRONG"
 			error_count += 1
-		else:	
-			print "This is CORRECT"
+		#else:	
+			#print "This is CORRECT"
 		
 		index += 1
 		tries += 1
-		print "STATISTICS:"
-		print "errors: " + str(error_count) + " out of: " + str(tries) + " remaining tests: " + str(len(typos)-tries)
-		error = (float(error_count)/float(tries))
-		error_individual[index-1] = error
-		rolling_window = shiftInPlace(rolling_window, 1)
-		rolling_window[-1] = error
-		print rolling_window
+		#print "STATISTICS:"
+		#print "errors: " + str(error_count) + " out of: " + str(tries) + " remaining tests: " + str(len(typos)-tries)
+		#error = (float(error_count)/float(tries))
+		#error_individual[index-1] = error
+		#rolling_window = shiftInPlace(rolling_window, 1)
+		#rolling_window[-1] = error
+		#print rolling_window
 
-		if ((max(rolling_window)-min(rolling_window) < 0.01) & (min(rolling_window) != 0)):
-			print "found correct number of trials: " + str(tries)
-			break
+		#if ((max(rolling_window)-min(rolling_window) < 0.01) & (min(rolling_window) != 0)):
+		#	print "found correct number of trials: " + str(tries)
+		#	break
 	
-	plt.plot([x for x in range(0,tries)], error_individual[0:tries])
-	plt.show()
+	#plt.plot([x for x in range(0,tries)], error_individual[0:tries])
+	#plt.show()
 	return float(error_count)/float(tries)
 		
 		
 	
 
 if __name__ == "__main__":
-	
 	if ((len(sys.argv) < 3) | (len(sys.argv) > 3)):
 		sys.exit( "Wrong number of command line arguments. \n Usage: python spellcheck.py <inputFileName> <dictionaryFile> ")
 	else:
@@ -158,11 +165,38 @@ if __name__ == "__main__":
 	#print typos
 	#print truewords
 	
+	#start = time.time()
+	#print measure_error(typos, truewords, dictionary)
+	#print "----------------------------"
+	#print "----------------------------"
+	#print "Time: " + str(time.time() -start)
+
+
+	## setup the sample set of 193 typos and truewords
+	idx = random.randint(0, len(typos)-193)
+	typos = typos[idx:idx+193]
+	truewords = truewords[idx:idx+193]
+	print "starting parameter test"
+	setting_iC = 0
+	setting_dC = 0
+	setting_sC = 0
+	parameters = [1,2,4]
+	min_error = 1
 	start = time.time()
-	print measure_error(typos, truewords, dictionary)
-	print "----------------------------"
-	print "----------------------------"
+	for insertionCost in parameters:
+		for deletionCost in parameters:
+			for substitutionCost in parameters:
+				error_local = measure_error(typos, truewords, dictionary)
+				print "----------- NEW SETTING ----------------"
+				if (error_local < min_error):
+					min_error = error_local
+					setting_iC = insertionCost
+					setting_sC = substitutionCost
+					setting_dC = deletionCost
+					print "updated to setup: sC = " + str(setting_sC) + " dC = " + str(setting_dC) + " iC = " + str(setting_iC) + " with error rate = " + str(error_local)
+				print "current setup: sC = " + str(substitutionCost) + " dC = " + str(deletionCost) + " iC = " + str(insertionCost) + " with error rate = " + str(error_local)
+
+	print "-----------------FINAL-----------------"
+	print "ideal setup: sC = " + str(setting_sC) + " dC = " + str(setting_dC) + " iC = " + str(setting_iC) + " with error rate = " + str(min_error)
 	print "Time: " + str(time.time() -start)
-	
-	
 	
