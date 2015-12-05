09c8f246656eb245f82b2e403e5e3a310673f2fa
MarcGyongyosi <marc.gyongyosi@u.northwestern.edu>
2015-12-04 19:48:57 -0600

    problem 1 complete

--- hw9/handout/problem1.py
+++ hw9/handout/problem1.py
@@ -3,7 +3,7 @@ import numpy as np
 from mnist import load_mnist
 
 
-
+"""
 ###PROBLEM 1A
 images, labels = load_mnist(digits=range(0,10), path = '.')
 print "total number of images: " + str (len(images))
@@ -12,5 +12,9 @@ for i in range(0,10):
     images, labels = load_mnist(digits=[i], path='.')
 
     print str(i) + " number of images: " + str(len(images))
-
-
+"""
+###PROBLEM 1B
+images, labels = load_mnist(digits=[0], path = '.')
+for i in range(0,50):
+    plt.imshow(images[i], cmap = 'gray')
+    plt.show()
