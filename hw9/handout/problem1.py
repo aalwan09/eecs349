51249bf0eeaa156b9954f99439c3647d23565ee0
MarcGyongyosi <marc.gyongyosi@u.northwestern.edu>
2015-12-04 19:06:21 -0600

    problem 1 a solved

--- hw9/handout/problem1.py
+++ hw9/handout/problem1.py
@@ -4,6 +4,9 @@ from mnist import load_mnist
 
 
 
+###PROBLEM 1A
+images, labels = load_mnist(digits=range(0,10), path = '.')
+print "total number of images: " + str (len(images))
 
 for i in range(0,10):
     images, labels = load_mnist(digits=[i], path='.')
