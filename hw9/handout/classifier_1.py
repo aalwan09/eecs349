59e4dd9dbe8391bdbe64e389820d967c177207e9
Gautier Dagan <gautierdagan2017@u.northwestern.edu>
2015-12-05 20:39:41 -0600

    cleaned and all in one file for classify_1

--- hw9/handout/classifier_1.py
+++ hw9/handout/classifier_1.py
@@ -4,8 +4,70 @@ from sklearn import svm # this is an example of using SVM
 from mnist import load_mnist
 import matplotlib.pyplot as plt
 import numpy as np
-from experiments import fairtraintest
 
+#fairtraintest return Training and Testing Arrays of size trainsize and testsize (x28x28)
+#it selects example so as to have an equal distribution of each label - You can also put 0 for testsize or trainsize, and then it just returns an equal distribution of labels
+def fairtraintest(images, labels, trainsize, testsize): 
+    if len(images) < trainsize + testsize:
+        print "error: cannot divide images without repeats"
+        return False
+    else: 
+        indexes = np.arange(len(images))
+        np.random.shuffle(indexes) #randomize data selected
+        
+        done = False
+
+        doneTest = np.zeros(10, dtype=bool)
+        Testcounts = np.zeros(10)
+        Testtotalcount = 0
+        
+        TestingSet = np.zeros((testsize, 784))
+        TestingLabels = np.zeros(testsize)
+
+        if(testsize == 0):
+            doneTest = np.ones(10, dtype=bool)
+
+        doneTraining = np.zeros(10, dtype=bool)
+        Traincounts = np.zeros(10)
+        Traintotalcount = 0
+
+        TrainingSet = np.zeros((trainsize, 784))
+        TrainLabels = np.zeros(trainsize)
+
+        if(trainsize == 0):
+            doneTraining = np.ones(10, dtype=bool)
+
+        count = 0
+        while done == False:
+            label = labels[indexes[count]]
+            if doneTest[label] == False and Testtotalcount < testsize and testsize > 0:
+                TestingSet[Testtotalcount] = images[indexes[count]]
+                TestingLabels[Testtotalcount] = label
+                Testtotalcount += 1
+                Testcounts[label] += 1
+
+                if Testcounts[label] >= (testsize/10):
+                    doneTest[label] = True
+
+            elif doneTraining[label] == False and Traintotalcount < trainsize and trainsize > 0:
+                TrainingSet[Traintotalcount] = images[indexes[count]]
+                TrainLabels[Traintotalcount] = label
+
+                Traintotalcount += 1
+                Traincounts[label] += 1 
+
+                if Traincounts[label] >= (trainsize/10):
+                    doneTraining[label] = True
+
+            if np.all(doneTest) and np.all(doneTraining):
+                done = True
+
+            count += 1 
+
+            if count == len(images):
+                done = True
+
+        return TrainingSet, TrainLabels, TestingSet, TestingLabels
 
 def preprocess(images):
     pass
@@ -30,13 +92,12 @@ def build_classifier(images, labels, c, deg, poly):
     return classifier
 
 ##the functions below are required
-def save_classifier(classifier, training_set, training_labels):
+def save_classifier(classifier):
     #this saves the classifier to a file "classifier" that we will
     #load from. It also saves the data that the classifier was trained on.
     import pickle
     pickle.dump(classifier, open('classifier_1.p', 'w'))
-    pickle.dump(training_set, open('training_set_1.p', 'w'))
-    pickle.dump(training_labels, open('training_labels_1.p', 'w'))
+
 
 
 def classify(images, classifier):
@@ -171,17 +232,13 @@ if __name__ == "__main__":
     #picking training and testing set for optimizing SVM
 
     #SelectedImages, SelectedLabels, temp, temp2 = fairtraintest(images, labels, 1000, 0)
+
     import pickle
     #pickle.dump(SelectedImages, open('training_set_1.p', 'w'))
     #pickle.dump(SelectedLabels, open('training_labels_1.p', 'w'))
 
     SelectedImages = pickle.load(open('training_set_1_final.p'))
     SelectedLabels = pickle.load(open('training_labels_1_final.p'))
-
-
-
-    #TrainingSizeFold(SelectedImages, SelectedLabels)
-
 
 
     #Training on C coefficient
@@ -194,5 +251,6 @@ if __name__ == "__main__":
     #fivefoldconfusion(SelectedImages, SelectedLabels, 50, 2, 1)
 
     #Error = fivefoldsize(SelectedImages, SelectedLabels, , 3)
-    #save_classifier(classifier, training_set, training_labels)
+    #classifier = build_classifier(SelectedImages, SelectedLabels, 50, 2, 1)
+    #save_classifier(classifier)
     
